37b2471dae427f574c2243cd9d37754e81cfd33f
Rui Martins <91401714+skuat3@users.noreply.github.com>
2022-08-07 15:33:00 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+
+const API_KEY = process.env.API_KEY;
+const API_Ending= process.env.API_Ending;
+const API_ID= process.env.API_ID;
+
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+const https = require("https");
+const client = require("@mailchimp/mailchimp_marketing"); 
+ 
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+ 
+
+ 
+client.setConfig({
+  apiKey: API_KEY,
+  server: API_Ending,
+});
+
+ 
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/", function(req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+  console.log(firstName, lastName, email);
+  const subscribingUser = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email
+  }
+ 
+  const run = async () => {
+    try {
+      const response = await client.lists.addListMember(API_ID, {
+        email_address: subscribingUser.email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: subscribingUser.firstName,
+          LNAME: subscribingUser.lastName
+        }
+      });
+      console.log(response);
+      res.sendFile(__dirname + "/success.html");
+    } catch (err) {
+      console.log(err.status);
+      res.sendFile(__dirname + "/failure.html");
+    }
+  };
+ 
+  run();
+});
+ 
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+});
+ 
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running on port 3000.");
+});
